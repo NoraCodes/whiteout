036e79d2b8f3fd529dae02db2f9d23a540e3dc62
Leonora Tindall <nora@nora.codes>
2019-09-27 10:15:11 -0500

    Fix examples and add examples in README

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 [![crates.io badge](https://img.shields.io/crates/v/whiteout.svg)](https://crates.io/crates/whiteout) [documentation](https://docs.rs/whiteout/)  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
 
-`whiteout` provides macros that erase the type of any value into an `impl Trait` for a given trait. Obviously, this requires `#![feature(conservative_impl_trait)]` to be enabled on your crate root.
+`whiteout` provides macros that erase the type of any value into an `impl Trait` for a given trait. This gives you a value that can only be use with the methods of that trait, and whose type is unnameable.
+
+```rust
+#[macro_use] extern crate whiteout;
+fn main() {
+    let a = erase!(10, std:ops::Add<i64, Output=i64>);
+    assert_eq!(a + 10, 20);
+}
+```
+Since we sometimes want to use these values together, `whiteout` provides both a one-time macro and a macro that produces a function which returns a consistent unnameable type, allowing multiple erased values to be used in conjunction. See the documentation for more info.
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,39 @@
 //! `whiteout` provides macros that erase the type of any value into 
-//! an `impl Trait` for a given trait. Obviously, this requires 
-//! `#![feature(conservative_impl_trait)]` to be enabled on your
-//! crate root.
+//! an `impl Trait` for a given trait.
 //!
 //! # Examples
 //!
 //! Single values can be erased using the `erase!` macro.
 //!
 //! ```
-//! #![feature(conservative_impl_trait)]
-//! #[macro_use]
-//! extern crate whiteout;
-//! fn main() {
-//!     let a = erase!(10, std::ops::Add<i64, Output=i64>);
-//!     assert_eq!(a + 10, 20);
-//! }
+//!# #[macro_use]
+//!# extern crate whiteout;
+//!# fn main() {
+//! let a = erase!(10, std::ops::Add<i64, Output=i64>);
+//! let b = erase!(5, std::ops::Add<i64, Output=i64>);
+//! assert_eq!(a + 10, 20);
+//! assert_eq!(b + 10, 15);
+//! // This fails, the types are opaque
+//! // assert_eq!(a + b, 15);
+//!# }
 //! ```
 //!
 //! These erased values can't be used together, though; they have different
 //! anonymized types. Therefore, you sometimes need the `eraser!` macro.
 //!
 //! ```
-//! #![feature(conservative_impl_trait)]
 //! #[macro_use]
 //! extern crate whiteout;
 //!
-//! // Define a custom trait and a blanket impl.
+//! // Define a custom trait into which types will be erased.
 //! trait MyTrait: 
-//!     std::ops::Add<Self, Output=Self> 
-//!     + std::convert::From<i32> 
-//!     + std::fmt::Debug 
-//!     + PartialEq 
+//!     std::ops::Add<Self, Output=Self>  // Allow the operation we need
+//!     + std::convert::From<i32>  // Allow converting from concrete values
+//!     + std::fmt::Debug  // Allow printing (for use with assert!())
+//!     + PartialEq  // Allow comparison (for use with assert_eq!())
 //!     {}
 //!
+//! // Implement MyTrait for all possible types.
 //! impl<T> MyTrait for T 
 //!     where T: std::ops::Add<Self, Output=Self> 
 //!     + std::convert::From<i32> 
@@ -40,7 +41,7 @@
 //!     + PartialEq
 //!     {}
 //!
-//! // Create an eraser function for our custom trait
+//! // Create an eraser function for the custom trait
 //! eraser!(erase_my_trait, MyTrait);
 //!
 //! fn main() {
@@ -48,14 +49,13 @@
 //!     // If we used erase!(10, MyTrait); for these
 //!     // they would be of different types.
 //!     let a = erase_my_trait(10);
-//!     let b = erase_my_trait(10);
-//!     assert_eq!(a + b, 20.into());
+//!     let b = erase_my_trait(5);
+//!     assert_eq!(a + b, 15.into());
 //! }
 //! ```
 //!
 //!
 
-#![feature(conservative_impl_trait)]
 
 /// `eraser!(name, trait)` creates a function with the given identifier that 
 /// erases values to an anonymous type that is `impl Trait` for the given trait
