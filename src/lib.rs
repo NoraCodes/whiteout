98d09b4607c262f020525a46e79eec720f4a9264
Leonora Tindall <nora@nora.codes>
2019-10-12 10:35:47 -0500

    Copy documentation onto appropriate items
    This makes it easier to refer to them.

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,44 @@
 
 
 /// `eraser!(name, trait)` creates a function with the given identifier that 
-/// erases values to an anonymous type that is `impl Trait` for the given trait
+/// erases values to an anonymous type that is `impl Trait` for the given trait.
+///
+/// # Examples
+///
+/// ```
+/// #[macro_use]
+/// extern crate whiteout;
+///
+/// // Define a custom trait into which types will be erased.
+/// trait MyTrait: 
+///     std::ops::Add<Self, Output=Self>  // Allow the operation we need
+///     + std::convert::From<i32>  // Allow converting from concrete values
+///     + std::fmt::Debug  // Allow printing (for use with assert!())
+///     + PartialEq  // Allow comparison (for use with assert_eq!())
+///     {}
+///
+/// // Implement MyTrait for all possible types.
+/// impl<T> MyTrait for T 
+///     where T: std::ops::Add<Self, Output=Self> 
+///     + std::convert::From<i32> 
+///     + std::fmt::Debug
+///     + PartialEq
+///     {}
+///
+/// // Create an eraser function for the custom trait
+/// eraser!(erase_my_trait, MyTrait);
+///
+/// fn main() {
+///     // Use the eraser function. 
+///     // If we used erase!(10, MyTrait); for these
+///     // they would be of different types.
+///     let a = erase_my_trait(10);
+///     let b = erase_my_trait(5);
+///     assert_eq!(a + b, 15.into());
+/// }
+/// ```
+///
+///
 #[macro_export]
 macro_rules! eraser {
     ($name:ident, $($tr:tt)*) => {
@@ -70,8 +107,26 @@ macro_rules! eraser {
     }
 }
 
+
 /// `erase!(value, trait)` turns a value of any type that implements trait into 
 /// an erasted type which is `impl Trait` for that trait.
+/// 
+/// # Examples
+///
+///
+/// ```
+///# #[macro_use]
+///# extern crate whiteout;
+///# fn main() {
+/// let a = erase!(10, std::ops::Add<i64, Output=i64>);
+/// let b = erase!(5, std::ops::Add<i64, Output=i64>);
+/// assert_eq!(a + 10, 20);
+/// assert_eq!(b + 10, 15);
+/// // This fails, the types are opaque
+/// // assert_eq!(a + b, 15);
+///# }
+/// ```
+///
 #[macro_export]
 macro_rules! erase {
     ($val:expr, $($tr:tt)*) => {
